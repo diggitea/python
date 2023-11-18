348b304e489377ba98d180f6d95c7304dd20e265
DiggityDorben <56617089+DiggityDorben@users.noreply.github.com>
2023-11-17 21:49:01 -0600

    Create test_television.py

---
+++ test_television.py
@@ -0,0 +1,10 @@
+import unittest
+from television import *
+
+class MyTestCase(unittest.TestCase):
+    def test__innit__(self):
+        tv_1 = Television()  # add assertion here
+
+
+if __name__ == '__main__':
+    unittest.main()
